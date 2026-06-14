1f1f5f1eaf2a6a86df63eebe0b75e838e00e415c
Emil Storgaard <est@novicell.dk>
2026-06-14 22:57:55 +0200

    Fix cors

--- src/routes/overblik/+page.ts
+++ src/routes/overblik/+page.ts
@@ -1,8 +1,6 @@
 import { dashboardApi } from '$lib/api/dashboard';
 import type { PageLoad } from './$types';
 
-export const ssr = false;
-
 export const load: PageLoad = async ({ fetch }) => {
   const dashboard = await dashboardApi.get(fetch);
   return { dashboard };
